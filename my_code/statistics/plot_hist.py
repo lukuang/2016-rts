2456ae99acb43fd269abcb67b5fdeb349e6943bf
lukuang <lukuang1989@gmail.com>
2016-06-23 14:13:36 -0400

    add code to plot histogram of tweets

--- my_code/statistics/plot_hist.py
+++ my_code/statistics/plot_hist.py
@@ -18,7 +18,7 @@ from process_tweet.tweet_proc import *
 class TimeBins(object):
     def __init__(self, gap=3600,start=START15,end=END15):
         self.start,self.end = start,end
-        self.bin_size = (self.end.epoch-self.start.epoch)/gap
+        self.bin_size = (self.end.epoch-self.start.epoch)/gap + 1
         self.bins = [0]*self.bin_size
         print "The size is: %d" %(self.bin_size)
 
