a6eba73d3be80826e8c8f53735facf2f57ba2609
lukuang <lukuang1989@gmail.com>
2016-06-29 16:35:02 -0400

    get statistics from index

--- my_code/distribution/indri_code/get_statistics.cpp
+++ my_code/distribution/indri_code/get_statistics.cpp
@@ -48,7 +48,8 @@ void get_statistics(indri::collection::Repository& r,map<string, int>& cf,
 void output(map<string, int>& cf, map<string, int> df,int& n,string& dest_dir){
     
     string end_str = "/";
-    if (end_str.compare(dest_dir[dest_dir.length() - 1])!=0){
+    const string for_test = dest_dir;
+    if (end_str.compare(for_test[for_test.length() - 1])!=0){
         dest_dir += end_str;
     }
 
