441d92c4a9e71772ee5714857eed30ea36899b64
lukuang <lukuang1989@gmail.com>
2016-06-30 17:11:01 -0400

    estimate aupr for a rank

--- my_code/distribution/misc.py
+++ my_code/distribution/misc.py
@@ -8,5 +8,5 @@ import numpy as np
 def compute_stat_from_list(score_list):
     temp = np.array(score_list)
     mean = np.mean(temp)
-    var = np.var(a)
+    var = np.var(temp)
     return mean,var
