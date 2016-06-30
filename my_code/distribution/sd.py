368b0a1da79bed027bd940dcd635a93b20075098
lukuang <lukuang1989@gmail.com>
2016-06-30 17:13:03 -0400

    estimate aupr for a rank

--- my_code/distribution/sd.py
+++ my_code/distribution/sd.py
@@ -19,7 +19,7 @@ def compute_idf_avg(stats,query):
 def compute_scq_avg(stats,query):
     scq_avg = .0
     for w in query:
-        scq_avg += 1+ math.log(self.cf[w])*math.log( 1 + self.idf[w] )
+        scq_avg += 1+ math.log(stats.cf[w])*math.log( 1 + stats.idf[w] )
 
     scq_avg /= len(query)
     return scq_avg
