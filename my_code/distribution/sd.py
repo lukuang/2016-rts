95c1c66673b7803d9fb6b0234fc452a93b0f421a
lukuang <lukuang1989@gmail.com>
2016-06-30 17:01:01 -0400

    estimate aupr for a rank

--- my_code/distribution/sd.py
+++ my_code/distribution/sd.py
@@ -152,7 +152,7 @@ class SD(object):
             for qid in self._run.ranking:
                 self._aupr[lambda_string][qid]
                 lambda_value = self._lambda[lambda_string][qid]
-                self._aupr[lambda_string][qid] = 
+                self._aupr[lambda_string][qid] = \
                         self._compute_aupr(qid,lambda_value)
 
     @property
