20b688f9d464aab249fdb6efc629c0417956757a
Kuang Lu <lukuang@g115.farber.hpc.udel.edu>
2016-07-10 12:00:36 -0400

    debug

--- my_code/baseline/2015/tweet2text_all.py
+++ my_code/baseline/2015/tweet2text_all.py
@@ -11,22 +11,21 @@ import codecs
 
 from myUtility.misc import split_list
 
-from ..process_tweet import CrawledTweetTrecTextFactory,CrawledTweet
-
+sys.path.append("/home/1546/code/2016-rts/my_code")
+from process_tweet import CrawledTweetTrecTextFactory,CrawledTweet
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("dest_dir")
     parser.add_argument("source_dir")
+    parser.add_argument("dest_dir")
     parser.add_argument("num_of_run",type=int)
     parser.add_argument("run_id",type=int)
     args=parser.parse_args()
 
-    all_files = [os.path(args.source_dir,f) for f in os.walk(args.source_dir).next()[2] ]
+    all_files = [os.path.join(args.source_dir,f) for f in os.walk(args.source_dir).next()[2] ]
 
     sub_list = split_list(all_files,args.num_of_run,args.run_id)
-    print "process %d out of %d files" %(len(all_files),len(sub_list))
     for tweet_file in sub_list:
         text_writer = CrawledTweetTrecTextFactory(tweet_file,args.source_dir)
         text_writer.write()
