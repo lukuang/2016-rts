71205e3b156d9e02b191cf86611bc68a1b8781a1
lukuang <lukuang1989@gmail.com>
2016-07-10 11:17:16 -0400

    generate trec text from crawled tweets(uncompresssed)

---
+++ my_code/process_tweet/__init__.py
@@ -0,0 +1,6 @@
+from process_crawled_tweets import CrawledTweetTrecTextFactory,CrawledTweet
+
+__all__=[
+        "CrawledTweet",
+        "CrawledTweetTrecTextFactory"
+        ]
---
+++ my_code/process_tweet/process_crawled_tweets.py
@@ -0,0 +1,110 @@
+"""
+process crawled tweets
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+import codecs
+import time
+import string
+
+from myUtility.indri import TextFactory
+from tweet_proc import *
+
+
+def convert_epoch_to_indri_date(epoch_string):
+
+    struct = time.gmtime(epoch_string)
+    time_string = "%s/%s/%d" %(string.zfill(str(struct.tm_mon),2),string.zfill(str(struct.tm_mday),2),struct.tm_year)
+    return time_string
+
+
+
+class CrawledTweet(Tweet):
+    """Struct for storing information of a single tweet
+    for crawled tweet. Fields it contains are:
+        id,
+        text,
+        created_at,
+        timestamp_ms
+    """
+    def __init__(self,tweet_data):
+        timestamp_ms = tweet_data["timestamp_ms"]
+        created_at = tweet_data["created_at"]
+        tid = tweet_data["id_str"]
+        text = tweet_data["text"]
+        timestamp_s = float(timestamp_ms)/1000
+        date_string = convert_epoch_to_indri_date(timestamp_s)
+        super(CrawledTweet,self).__init__(tid,text)
+        self._created_at,self._timestamp_ms, self._date_stirng = \
+            created_at,timestamp_ms,date_string
+        self._add_extra_field()
+
+    def _add_extra_field(self):
+        """get extra field from the crawled tweet
+        """
+        self.extra_fields = ["created_at","timestamp_ms","date"]
+        self.field_data = {
+                        "created_at" : self._created_at,
+                        "timestamp_ms" : self._timestamp_ms,
+                        "date" : self._date_stirng
+                        }
+    
+    
+
+
+
+class CrawledTweetTrecTextFactory(TextFactory):
+    """Build trect text for crawled tweets
+    """
+    def __init__(self,source_file,dest_dir,debug=False):
+        dest_file_path = self._get_dest_file_path(source_file,dest_dir)
+        
+        super(CrawledTweetTrecTextFactory,self).__init__(dest_file_path)
+
+        self._source_file,self.debug = source_file,debug
+
+
+    def _get_dest_file_path(self,source_file,dest_dir):
+        file_name = os.path.basename(source_file)
+        dest_file_path = os.path.join(dest_dir,file_name)
+        return dest_file_path
+
+
+    def write(self):
+        self._process_file()
+        super(CrawledTweetTrecTextFactory,self).write()
+
+
+    def _process_file(self):
+        with codecs.open(self._source_file,"r","utf-8") as f:
+            for line in f:
+                line = line.rstrip()
+                if len(line)!=0:
+                    self._process_line(line)
+
+
+    def _process_line(self,line):
+        tweet_data = json.loads(line)
+        if "delete" not in tweet_data:
+            tweet = CrawledTweet(tweet_data)
+            # print tweet.field_data
+            # print tweet.tid
+            # print tweet.text
+            self.add_document(tweet.tid,tweet.text,
+                              tweet.extra_fields,
+                              tweet.field_data)
+        
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("")
+    args=parser.parse_args()
+
+if __name__=="__main__":
+    main()
+
