a0256659a03c23a2c1b5535b35027412c3eeec02
lukuang <lukuang1989@gmail.com>
2016-07-12 20:22:55 -0400

    change code for run tunning queries and write result for same run to one file

--- my_code/baseline/2015/run_query.py
+++ my_code/baseline/2015/run_query.py
@@ -20,51 +20,56 @@ def run_query(query_file,result_file,debug):
     else:
         p = subprocess.Popen(run_args,stdout=subprocess.PIPE)
         output = p.communicate()[0]
-        with open(result_file,"w") as of:
+        with open(result_file,"a") as of:
                 of.write(output)
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("top_result_dir")
-    parser.add_argument("top_query_para_dir")
+    parser.add_argument("result_dir")
+    parser.add_argument("query_para_dir")
     parser.add_argument("--debug","-de",action="store_true")
     
     
     args=parser.parse_args()
-    index_methods = ["individual","incremental"]
+    # index_methods = ["individual","incremental"]
 
-    expansion_methods = [
-        "original",
-        "snippet",
-        "wiki",
-        "pseudo"
-    ]
+    # expansion_methods = [
+    #     "original",
+    #     "snippet",
+    #     "wiki",
+    #     "pseudo"
+    # ]
 
-    not_implemented = ["snippet","wiki"]
+    # not_implemented = ["snippet","wiki"]
 
     
 
 
-    for index_method in index_methods:
-        for expansion_method in expansion_methods:
-            if expansion_method not in not_implemented:
-                sub_query_dir = os.path.join(
-                                    args.top_query_para_dir,
-                                    index_method,
-                                    expansion_method,
-                                )
-                sub_result_dir = os.path.join(
-                                    args.top_result_dir,
-                                    index_method,
-                                    expansion_method,
-                                )
-                file_names =  os.walk(sub_query_dir).next()[2]
-                for file_name in file_names:
+    # for index_method in index_methods:
+    #     for expansion_method in expansion_methods:
+    #         if expansion_method not in not_implemented:
+    #             sub_query_dir = os.path.join(
+    #                                 args.top_query_para_dir,
+    #                                 index_method,
+    #                                 expansion_method,
+    #                             )
+    #             sub_result_dir = os.path.join(
+    #                                 args.top_result_dir,
+    #                                 index_method,
+    #                                 expansion_method,
+    #                             )
+    file_names =  os.walk(sub_query_dir).next()[2]
+    for file_name in file_names:
 
-                    query_file = os.path.join(sub_query_dir,file_name)
-                    result_file = os.path.join(sub_result_dir,file_name)
-                    run_query(query_file,result_file,args.debug)
+        query_file = os.path.join(sub_query_dir,file_name)
+        #result_file = os.path.join(sub_result_dir,file_name)
+        m = re.search("^(\d+)_(.+)$",file_name)
+        result_file = os.path.join(sub_result_dir,m.group(2))
+        if m is not None:   
+            run_query(query_file,result_file,args.debug)
+        else:
+            print "Wrong file name %s" %file_name
 
 
 if __name__=="__main__":
