7aa19fbcfbbbc7c6c5068312234c9173f8e6d151
lukuang <lukuang1989@gmail.com>
2016-07-13 11:22:59 -0400

    change to generating one index for all queries

--- my_code/baseline/snippets/snippet_build_index.py
+++ my_code/baseline/snippets/snippet_build_index.py
@@ -1,5 +1,5 @@
 """
-generate index para file for each query
+generate index para file for  snippets (one index for all queries)
 """
 
 import os
@@ -19,13 +19,14 @@ def main():
     parser.add_argument("index_para_dir")
 
     args=parser.parse_args()
-
+    file_list = []
     for qid in os.walk(args.trec_text_dir).next()[2]:
-        file_list = [os.path.join(args.trec_text_dir,qid)]
-        index_para_file = os.path.join(args.index_para_dir,qid)
-        index_path = os.path.join(args.index_dir,qid)
-        gene_indri_index_para_file(file_list,index_para_file,
-                    index_path)
+        file_list.append(os.path.join(args.trec_text_dir,qid))
+        
+    index_para_file = os.path.join(args.index_para_dir,"index_para")
+    index_path = args.index_dir
+    gene_indri_index_para_file(file_list,index_para_file,
+                index_path)
 
 if __name__=="__main__":
     main()
