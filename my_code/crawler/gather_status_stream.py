f864f32a8bf71c0cbcb2d8feba3ab989d46870a9
lukuang <lukuang1989@gmail.com>
2016-07-18 13:47:17 -0400

    add text logger to write text to disk

--- my_code/crawler/gather_status_stream.py
+++ my_code/crawler/gather_status_stream.py
@@ -17,27 +17,36 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from datetime import datetime
-import argparse,json
+import argparse,json,os
 import logging
 import logging.handlers
 
+from myUtility.misc import gene_single_indri_text
+
+from ..process_tweet.process_crawled_tweet import CrawledTweet
 
 
 
 
 class TweetListener(StreamListener):
 
-  def __init__(self,text_dir,api=None):
+  def __init__(self,log_dir,text_dir,api=None):
     super(TweetListener,self).__init__(api)
     self.logger = logging.getLogger('tweetlogger')
-    self._text_dir = text_dir
+    self.text_loger = logging.getLogger("textlogger")
+
+    self._log_dir,self._text_dir = log_dir,text_dir
+
     now = datetime.utcnow()
-    statusHandler = logging.handlers.TimedRotatingFileHandler('status.log',when='M',encoding='utf-8',utc=True)
+    statusHandler = logging.handlers.TimedRotatingFileHandler(os.path.join(self._log_dir,'status.log'),when='M',encoding='utf-8',utc=True)
     statusHandler.setLevel(logging.INFO)
     self.logger.addHandler(statusHandler)
     
+    textHandler = logging.handlers.TimedRotatingFileHandler(os.path.join(self._text_dir,'status.log'),when='M',encoding='utf-8',utc=True)
+    textHandler.setLevel(logging.INFO)
+    self.text_loger.addHandler(textHandler)
 
-    warningHandler = logging.handlers.TimedRotatingFileHandler('warning.log',when='H',encoding='bz2',utc=True)
+    warningHandler = logging.handlers.TimedRotatingFileHandler(os.path.join(self._log_dir,'warning.log'),when='H',encoding='bz2',utc=True)
     warningHandler.setLevel(logging.WARN)
     self.logger.addHandler(warningHandler)
     logging.captureWarnings(True);
@@ -55,6 +64,14 @@ class TweetListener(StreamListener):
     self.logger.info(data)
     if self.count % 1000 == 0:
         print "%d statuses processed" % self.count
+    if "delete" not in data:
+      tweet = CrawledTweet(data)
+      single_document_text = gene_single_indri_text(
+                                  tweet.tid,tweet.text,
+                                  tweet.extra_fields,
+                                  tweet.field_data)
+      self.textlogger.info(single_document_text)
+
     return True
 
   def on_error(self,exception):
@@ -64,9 +81,10 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument("auth_file")
   parser.add_argument("text_dir")
+  parser.add_argument("log_dir")
   args=parser.parse_args()
 
-  listener = TweetListener(args.text_dir)
+  listener = TweetListener(args.log_dir,args.text_dir)
   auth_info = json.load(open(args.auth_file))
   consumer_key = auth_info["consumer_key"]
   consumer_secret = auth_info["consumer_secret"]
