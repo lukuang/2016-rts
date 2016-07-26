5b880c75afa4c93b3c890fd92f904bb90d1a98a3
Lu Kuang <lukuang@infolab.ece.udel.edu>
2016-07-26 10:21:18 -0400

    convert indri query files generated by run.py to the format that can be used to get run result on any indri index

---
+++ my_code/distribution/query_prediction/convert_query.py
@@ -0,0 +1,66 @@
+"""
+convert indri query to 'qid: text' format
+"""
+
+import os
+import json
+import sys
+import re
+import argparse
+import codecs
+
+
+def get_judged_qid(qrel_file):
+    judged_qids = []
+    with open(qrel_file) as f:
+        for line in f:
+            parts= line.rstrip().split()
+            qid = parts[0]
+            if qid not in judged_qids:
+                judged_qids.append(qid)
+    return judged_qids
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("source_file")
+    parser.add_argument("dest_file")
+    parser.add_argument("--qrel_file","-qf",
+        default="/infolab/node4/lukuang/2015-RTS/2015-data/new_qrels.txt")
+    args=parser.parse_args()
+
+
+    judged_qids = get_judged_qid(args.qrel_file)
+
+    queries = {}
+    
+    with open(args.source_file) as f:
+        for line in f:
+            line = line.rstrip()
+            m = re.search("<number>(MB\d+)</number>",line)
+            if m:
+                qid = m.group(1)
+            m2 = re.search("<text>(.+?)</text>",line)
+            if m2:
+                query_string = m2.group(1)
+                m3 = re.search("\#combine\((.+?)\)",line)
+                if m3:
+                    query_string = m3.group(1)
+                m3 = re.search("\#weight\((.+?)\)",line)
+                if m3:
+                    query_string = m3.group(1)
+                    query_string = re.sub("[^a-zA-z\_\s]","",query_string)
+                query_string = re.sub("\s+"," ",query_string)
+                query_string = query_string.strip()
+                if qid in judged_qids:
+                    queries[qid] = query_string
+
+    with open(args.dest_file, 'w') as f:
+        for qid in queries:
+            f.write("%s:%s\n" %(qid, queries[qid]))
+
+
+
+if __name__=="__main__":
+    main()
+
