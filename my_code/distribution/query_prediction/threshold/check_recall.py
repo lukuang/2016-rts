74e7addf4584a6bd3b05d4ced911b0c88e8309d4
Lu Kuang <lukuang@infolab.ece.udel.edu>
2016-12-18 16:23:33 -0500

    check ndcgs(w/ and w/o recdundancy)

--- my_code/distribution/query_prediction/threshold/check_recall.py
+++ my_code/distribution/query_prediction/threshold/check_recall.py
@@ -26,16 +26,22 @@ def main():
 
     t2day = T2Day(args.tweet2day_file)
     sema_cluster = SemaCluster(args.cluster_file,t2day)
-  
+    qrel = Qrel(args.qrel_file)
+
     date = '20'
     results = {
-        "MB324":["623014387402543108","622951330240266240","623026064340742144","623786646845161472","623941353752383490"]
+        "MB324":["623014387402543108","622951330240266240","622951330240266240","623026064340742144","623786646845161472","623941353752383490"]
         } 
     for i in range(len(results["MB324"])):
         print sema_cluster._day_cluster[date]["MB324"]
         sub_result = {"MB324":results["MB324"][:i+1]}
         recall = sema_cluster.day_cluster_recall(sub_result,date)
         print "now recall is %f" %recall
+        ndcg10 = qrel.day_dcg10_no_pre(date,sub_result,sema_cluster)
+        print "ndcg10 is %f" %(ndcg10)
+        ndcg10 = qrel.raw_ndcg10(date,sub_result,sema_cluster)
+        print "ndcg10 is %f" %(ndcg10)
+
 
 if __name__=="__main__":
     main()
