be0a9940496bdf00810ef61f2d2b8103425d7a11
Kuang Lu <lukuang1989@gmail.com>
2017-08-02 10:05:35 -0400

    change to use status log files instead

--- 2017/use_word_embedding/prepare_vector/prepare_text_file.py
+++ 2017/use_word_embedding/prepare_vector/prepare_text_file.py
@@ -38,39 +38,39 @@ def check_file_name(year,intermediate_days,file_name):
         return False
 
 
-def get_file_list(src_dir,year,intermediate_days):
+def get_file_list(src_dir):
     file_list = []
     for file_name in os.walk(src_dir).next()[2]:
-        if check_file_name(year,intermediate_days,file_name):
-            file_list.append( os.path.join(src_dir,file_name) )
+        file_list.append( os.path.join(src_dir,file_name) )
     return file_list
 
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("begin_month",type=int)
-    parser.add_argument("begin_date",type=int)
-    parser.add_argument("end_month",type=int)
-    parser.add_argument("end_date",type=int)
-    parser.add_argument("year",choices=["2015","2016","2017"],
-        help="""
-            Choose the year:
-                2015
-                2016
-                2017
-        """)
+    # parser.add_argument("begin_month",type=int)
+    # parser.add_argument("begin_date",type=int)
+    # parser.add_argument("end_month",type=int)
+    # parser.add_argument("end_date",type=int)
+    # parser.add_argument("year",choices=["2015","2016","2017"],
+    #     help="""
+    #         Choose the year:
+    #             2015
+    #             2016
+    #             2017
+    #     """)
     parser.add_argument("src_dir")
     parser.add_argument("dest_file")
     args=parser.parse_args()
 
 
-    year_value = int(args.year)
-    intermediate_days = find_inclusive_intermediate_days(
-                            begin_month,begin_date,
-                            end_month,end_date,year_value
-                        )
-    file_list = get_file_list(args.src_dir,args.year,intermediate_days)
+    # year_value = int(args.year)
+    # intermediate_days = find_inclusive_intermediate_days(
+    #                         begin_month,begin_date,
+    #                         end_month,end_date,year_value
+    #                     )
+    # file_list = get_file_list(args.src_dir,args.year,intermediate_days)
+    file_list = get_file_list(args.src_dir)
 
     text_string_for_all = ""
 
@@ -79,18 +79,12 @@ def main():
         with open(single_file) as f:
             print "Process file: %s" %(single_file)
             for line in f:
-                if not in_text:
-                    m = re.search("<TEXT>(.+)",line)
-                    if m is not None:
-                        in_text = True
-                        text_string_for_all += " "+m.group(1).strip()
-                else:
-                    m2 = re.search("^(.+?)</TEXT>",line)
-                    if m2 is not None:
-                        in_text = False
-                        text_string_for_all += " "+m2.group(1).strip()
-                    else:
-                        text_string_for_all += " "+line.strip()
+                line = line.strip()
+                if line:
+                    tweet = json.loads(line)
+                    text = re.sub("\n"," ",tweet["text"])
+                    text_string_for_all += " " + text
+                    
 
     with open(args.dest_file,"w") as of:
         of.write(text_string_for_all)
