7f74808bd6023e84a536b5b282e75c439cc7ff92
Kuang Lu <lukuang1989@gmail.com>
2017-08-18 11:22:40 -0400

    add stopword removal and fix bugs

--- 2017/use_word_embedding/model.py
+++ 2017/use_word_embedding/model.py
@@ -35,7 +35,9 @@ def load_glove_model(model_file):
     return model,dimension
 
 def load_word2vec_model(model_file):
-    model = gensim.models.KeyedVectors.load_word2vec_format(model_file, binary=True)
+    model = gensim.models.KeyedVectors.load_word2vec_format(model_file, 
+                                                            binary=True,
+                                                            unicode_errors="ignore")
     dimension = model.vector_size
     return model,dimension
 
@@ -46,28 +48,39 @@ class EmbeddingModel(object):
 
     def __init__(self,model_file,model_type):
         self._model_type = model_type
+        self._stopwords = stopwords.words('english')
+
         if model_type ==  ModelType.glove:
             self._model, self._dimension = load_glove_model(model_file)
         elif model_type == ModelType.word2vec:
             self._model, self._dimension = load_word2vec_model(model_file)
-        self._stopwords = nltk.corpus.stopwords.words('english')
         self._missing_words = set()
         self._total_words = set()
 
-    def get_sentence_vector(sentence):
-        sentence_vector = np.array([.0]*300)
+    def get_sentence_vector(self,sentence):
+        sentence_vector = np.array([.0]*self._dimension)
+        size = 0
         for w in sentence:
             if w not in self._stopwords:
-                self._total_words.add(w)
+                # self._total_words.add(w)
                 try:
                     sentence_vector += self._model[w]
+                    size += 1
                 except KeyError:
-                    self._missing_words.add(w)
-
+                    pass
+                    # self._missing_words.add(w)
+        if size!=0:
+            sentence_vector /= size
         return sentence_vector
 
-    def similarity(sentence_vector1,sentence_vector2):
+    def similarity(self,sentence_vector1,sentence_vector2):
         return 1 - cosine(sentence_vector1,sentence_vector2)
+
+
+    def print_unseen_words_info(self):
+
+        print "There are %d unseen words in %d total words" %(len(self._missing_words),
+                                                              len(self._total_words)  )
 
 
 
